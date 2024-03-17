e85824b5d058fbcec15571ec40c4268132611ee1
johnpgr <jp.greidinger@hotmail.com>
2024-03-16 21:50:21 -0300

    traduzindo bgl

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p /opt/minecraft/backups
 cd /opt/minecraft/backups
-tar -C /opt/minecraft/paper -czf $(date +"%Y-%m-%d").tar.gz  world world_nether world_the_end
+tar -C /opt/minecraft/server -czf $(date +"%Y-%m-%d").tar.gz  world world_nether world_the_end
 rm -f /opt/minecraft/backups/$(date --date='5 days ago' +"%Y-%m-%d").tar.gz
